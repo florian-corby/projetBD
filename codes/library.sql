e9444b6d58320c0d2387824257e9d590e46e6c8a
Florian <florian-1992@hotmail.fr>
2021-04-26 21:22:07 +0200

    J'ai refait le script de création de la base en utilisant le nouveau schéma relationnel

--- codes/library.sql
+++ codes/library.sql
@@ -2,30 +2,8 @@
 -- ================== Script de création BD ==================== --
 -- ============================================================= --
 
-DROP TABLE Theme CASCADE CONSTRAINTS;
-CREATE TABLE Theme (
-    theme VARCHAR2(250),
-    CONSTRAINT PK_Theme PRIMARY KEY (theme)
-);
 
-DROP TABLE Editor CASCADE CONSTRAINTS;
-CREATE TABLE Editor (
-    name VARCHAR2(250),
-    address VARCHAR2(250),
-    phone INT,
-    CONSTRAINT PK_Editor PRIMARY KEY (name)
-);
-
-DROP TABLE Author CASCADE CONSTRAINTS;
-CREATE TABLE Author (
-    id INT,
-    name VARCHAR2(250),
-    fst_name VARCHAR2(250),
-    address VARCHAR2(250),
-    birth DATE,
-    CONSTRAINT PK_Author PRIMARY KEY (id)
-);
-
+-- ********* RIGHTS ZONE ********* --
 DROP TABLE CatBorrower CASCADE CONSTRAINTS;
 CREATE TABLE CatBorrower (
     cat_borrower VARCHAR2(250),
@@ -34,11 +12,13 @@ CREATE TABLE CatBorrower (
     CONSTRAINT Check_Borrower_borrowingMax CHECK (borrowing_max > 0)
 );
 
+
 DROP TABLE CatDoc CASCADE CONSTRAINTS;
 CREATE TABLE CatDoc (
     cat_document VARCHAR2(250),
     CONSTRAINT PK_CatDoc PRIMARY KEY(cat_document)
 );
+
 
 DROP TABLE Rights CASCADE CONSTRAINTS;
 CREATE TABLE Rights (
@@ -53,72 +33,94 @@ CREATE TABLE Rights (
 );
 
 
+
+-- ********* DOCUMENT ZONE ********* --
+DROP TABLE Author CASCADE CONSTRAINTS;
+CREATE TABLE Author (
+    id INT,
+    name VARCHAR2(250),
+    fst_name VARCHAR2(250),
+    address VARCHAR2(250),
+    birth DATE,
+    CONSTRAINT PK_Author PRIMARY KEY (id)
+);
+
+
+DROP TABLE Editor CASCADE CONSTRAINTS;
+CREATE TABLE Editor (
+    name VARCHAR2(250),
+    address VARCHAR2(250),
+    phone INT,
+    CONSTRAINT PK_Editor PRIMARY KEY (name)
+);
+
+
+DROP TABLE Keyword CASCADE CONSTRAINTS;
+CREATE TABLE Keyword (
+    keyword VARCHAR2(250),
+    CONSTRAINT PK_Keyword PRIMARY KEY (keyword)
+);
+
+
+DROP TABLE Theme CASCADE CONSTRAINTS;
+CREATE TABLE Theme (
+    theme VARCHAR2(250),
+    CONSTRAINT PK_Theme PRIMARY KEY (theme)
+);
+
+
 DROP TABLE Document CASCADE CONSTRAINTS;
 CREATE TABLE Document (
     reference INT,
     title VARCHAR2(250),
-    key_word VARCHAR2(250),
-    author INT,
+    qte INT NOT NULL,
     editor VARCHAR2(250),
     theme VARCHAR2(250),
     category VARCHAR2(250) NOT NULL,
-    qte INT,
+    pages INT,
+    time INT,
+    format VARCHAR2(250),
+    nbSubtitles INT,
     CONSTRAINT PK_Document PRIMARY KEY (reference),
-    CONSTRAINT FK_Document_Author FOREIGN KEY (author) REFERENCES Author(id),
     CONSTRAINT FK_Document_Editor FOREIGN KEY (editor) REFERENCES Editor(name),
     CONSTRAINT FK_Document_Theme FOREIGN KEY (theme) REFERENCES Theme(theme),
     CONSTRAINT FK_Document_Category FOREIGN KEY (category) REFERENCES CatDoc(cat_document),
     CONSTRAINT Check_Document_qte CHECK (qte >= 0)
 );
 
-DROP TABLE Book CASCADE CONSTRAINTS;
-CREATE TABLE Book (
+
+DROP TABLE DocumentAuthors CASCADE CONSTRAINTS;
+CREATE TABLE DocumentAuthors (
     reference INT,
-    page INT,    
-    CONSTRAINT PK_Book PRIMARY KEY (reference),
-    CONSTRAINT FK_Book_Document FOREIGN KEY (reference) REFERENCES Document(reference),
-    CONSTRAINT Check_Book_page CHECK (page > 0)
+    author_id INT,
+    CONSTRAINT PK_DocumentAuthors PRIMARY KEY (reference, author_id),
+    CONSTRAINT FK_DocumentAuthors_reference FOREIGN KEY (reference) REFERENCES Document(reference),
+    CONSTRAINT FK_DocumentAuthors_author_id FOREIGN KEY (author_id) REFERENCES Author(id)
 );
 
-DROP TABLE Video CASCADE CONSTRAINTS;
-CREATE TABLE Video (
+
+DROP TABLE DocumentKeywords CASCADE CONSTRAINTS;
+CREATE TABLE DocumentKeywords (
     reference INT,
-    time INT,
-    format VARCHAR2(250),
-    CONSTRAINT PK_Video PRIMARY KEY (reference),
-    CONSTRAINT FK_Video_Document FOREIGN KEY (reference) REFERENCES Document(reference),
-    CONSTRAINT Check_Video_time CHECK (time > 0)
+    keyword VARCHAR2(250),
+    CONSTRAINT PK_DocumentKeywords PRIMARY KEY (reference, keyword),
+    CONSTRAINT FK_DocumentKeywords_reference FOREIGN KEY (reference) REFERENCES Document(reference),
+    CONSTRAINT FK_DocumentKeywords_keyword FOREIGN KEY (keyword) REFERENCES Keyword(keyword)
 );
 
-DROP TABLE DVD CASCADE CONSTRAINTS;
-CREATE TABLE DVD (
-    reference INT,
-    time INT,
-    CONSTRAINT PK_DVD PRIMARY KEY (reference),
-    CONSTRAINT FK_DVD_Document FOREIGN KEY (reference) REFERENCES Document(reference),
-    CONSTRAINT Check_DVD_time CHECK (time > 0)
-);
-
-DROP TABLE CD CASCADE CONSTRAINTS;
-CREATE TABLE CD (
-    reference INT,
-    time INT,
-    subtitle INT,    
-    CONSTRAINT PK_CD PRIMARY KEY (reference),
-    CONSTRAINT FK_CD_Document FOREIGN KEY (reference) REFERENCES Document(reference),
-    CONSTRAINT Check_CD_time CHECK (time > 0),
-    CONSTRAINT Check_CD_subtitle CHECK (subtitle >= 0)
-);
 
 DROP TABLE Copy CASCADE CONSTRAINTS;
 CREATE TABLE Copy (
     id INT,
-    aisleID INT,
+    aisleID INT NOT NULL,
     reference INT NOT NULL,    
     CONSTRAINT PK_Copy PRIMARY KEY (id),
     CONSTRAINT FK_Copy_Document FOREIGN KEY (reference) REFERENCES Document(reference)
 );
 
+
+
+-- ********* BORROWINGS ZONE ********* --
 DROP TABLE Borrower CASCADE CONSTRAINTS;
 CREATE TABLE Borrower (
     id INT,
@@ -131,6 +133,7 @@ CREATE TABLE Borrower (
     CONSTRAINT FK_Borrower_Category FOREIGN KEY (category) REFERENCES CatBorrower(cat_borrower)
 );
 
+
 DROP TABLE Borrow CASCADE CONSTRAINTS;
 CREATE TABLE Borrow (
     borrower INT,
@@ -141,5 +144,6 @@ CREATE TABLE Borrow (
     CONSTRAINT FK_Borrow_Borrower FOREIGN KEY (borrower) REFERENCES Borrower(id),
     CONSTRAINT FK_Borrow_Copy FOREIGN KEY (copy) REFERENCES Copy(id)
 );
+
 
 COMMIT;
