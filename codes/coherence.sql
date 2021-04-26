b33613746342c170ee0ab7433dc511d585384b9d
Florian <florian-1992@hotmail.fr>
2021-04-26 21:01:32 +0200

    Mise à jour du SEA et du SR selon les nouvelles contraintes du cahier des charges (et de ce qu'on sait faire par rapport aux triggers...)

--- codes/coherence.sql
+++ codes/coherence.sql
@@ -34,7 +34,54 @@ WHERE doc.reference = v.reference);
 
 --SELECT * FROM docCatAppearances;
 --SELECT doc.reference FROM docCatAppearances doc GROUP BY doc.reference HAVING Count(*) > 1;
+CREATE OR REPLACE TRIGGER tg_book
+AFTER INSERT ON Book
+DECLARE alreadyReferenced int;
+BEGIN
+    SELECT doc.reference INTO alreadyReferenced
+    FROM docCatAppearances doc 
+    GROUP BY doc.reference 
+    HAVING Count(*) <> 1;
     
+    if(alreadyReferenced is not null) then 
+         DELETE FROM Book WHERE Book.reference = alreadyReferenced; 
+         DBMS_OUTPUT.PUT_LINE('ERROR :> Document already belongs to another category');
+    end if;
+END;
+/
+
+CREATE OR REPLACE TRIGGER tg_cd
+AFTER INSERT ON CD
+DECLARE alreadyReferenced int;
+BEGIN
+    SELECT doc.reference INTO alreadyReferenced
+    FROM docCatAppearances doc 
+    GROUP BY doc.reference 
+    HAVING Count(*) <> 1;
+    
+    if(alreadyReferenced is not null) then 
+         DELETE FROM CD WHERE CD.reference = alreadyReferenced; 
+         DBMS_OUTPUT.PUT_LINE('ERROR :> Document already belongs to another category');
+    end if;
+END;
+/
+
+CREATE OR REPLACE TRIGGER tg_dvd
+AFTER INSERT ON DVD
+DECLARE alreadyReferenced int;
+BEGIN
+    SELECT doc.reference INTO alreadyReferenced
+    FROM docCatAppearances doc 
+    GROUP BY doc.reference 
+    HAVING Count(*) <> 1;
+    
+    if(alreadyReferenced is not null) then 
+         DELETE FROM DVD WHERE DVD.reference = alreadyReferenced; 
+         DBMS_OUTPUT.PUT_LINE('ERROR :> Document already belongs to another category');
+    end if;
+END;
+/
+
 CREATE OR REPLACE TRIGGER tg_video
 AFTER INSERT ON Video
 DECLARE alreadyReferenced int;
@@ -42,7 +89,7 @@ BEGIN
     SELECT doc.reference INTO alreadyReferenced
     FROM docCatAppearances doc 
     GROUP BY doc.reference 
-    HAVING Count(*) > 1;
+    HAVING Count(*) <> 1;
     
     if(alreadyReferenced is not null) then 
          DELETE FROM Video WHERE Video.reference = alreadyReferenced; 
@@ -51,11 +98,17 @@ BEGIN
 END;
 /
 
+
 --INSERT INTO Video(reference, time, format) VALUES (1, 160, 'MP4');
---COMMIT;
 
 
 -- ******* La catégorie du document correspond à la relation qui y réfère ******* --
+
+
+
+
+--INSERT INTO Document (reference, title, key_word, author, editor, theme, category, qte) VALUES (25, 'Linux 4ever', 'Systeme', 7, 'Eyrolles', 'informatique', 'Video', 8000);
+--INSERT INTO Book (reference, page) VALUES (25, 1750);
 
 
 
