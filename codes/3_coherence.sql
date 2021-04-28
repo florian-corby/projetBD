7320e56caac1668f32d0c269f615e9429bbf61c7
Berthelot Yann <yann.bth1@gmail.com>
2021-04-28 18:11:11 +0200

    plus que 3 triggers

--- codes/3_coherence.sql
+++ codes/3_coherence.sql
@@ -41,13 +41,16 @@ END;
 
 
 -- ******* Ajout => vérif quantité Document ******* --
---CREATE OR REPLACE TRIGGER tg_Document_IsQteZero
---BEFORE INSERT ON Document
---FOR EACH ROW
---BEGIN
---
---END;
---/
+CREATE OR REPLACE TRIGGER tg_Document_IsQteZero
+BEFORE INSERT ON Document
+FOR EACH ROW
+BEGIN
+if :new.qte <= 0
+then raise_application_error('-20001', 'A document can only be added with positive quantity');
+end if;
+END;
+/
+
 
 
 ---------------------------------------------------------------------------------
@@ -55,44 +58,62 @@ END;
 ---------------------------------------------------------------------------------
 
 -- ******* Ajout => Vérification nombre d'emprunts ******* --
---CREATE OR REPLACE TRIGGER tg_Borrow_VerifMaxBorrow
---BEFORE INSERT ON Borrow
---FOR EACH ROW
---BEGIN
---
---END;
---/
+CREATE OR REPLACE TRIGGER tg_Borrow_VerifMaxBorrow
+BEFORE INSERT ON Borrow
+FOR EACH ROW
+DECLARE
+    max_borrow catborrower.borrowing_max%type;
+    borrow_nb number;
+BEGIN
+    select catborrower.borrowing_max
+    into max_borrow
+    from catborrower
+    inner join borrower on borrower.category = catborrower.cat_borrower
+    where borrower.id = :new.borrower;
+    SELECT count(*)
+    into borrow_nb
+    from borrow
+    where :new.borrower = borrower;
+    if borrow_nb +1 > max_borrow
+    then raise_application_error('-20001','You cannot borrow any document yet') ;
+    end if;
+END;
+/
 
 
 -- ******* Ajout =>  Aucun retard en cours ******* --
---CREATE OR REPLACE TRIGGER tg_Borrow_VerifOverdues
---BEFORE INSERT ON Borrow
---FOR EACH ROW
---BEGIN
---
---END;
---/
+CREATE OR REPLACE TRIGGER tg_Borrow_VerifOverdues
+BEFORE INSERT ON Borrow
+FOR EACH ROW
+declare 
+return_d borrow.return_date%type;
+BEGIN
+    select return_date
+    into return_d
+    from borrow
+    where borrow.borrower = :new.borrower;
+    if (return_d is null and return_d > sysdate)
+    then raise_application_error('-20001','You are late');
+    end if;
+END;
+/
 
 
 -- ******* Ajout =>  On ne peut pas l'emprunter si en cours d'emprunt ******* --
---CREATE OR REPLACE TRIGGER tg_Borrow_VerifIsBeingBorrowed
---BEFORE INSERT ON Borrow
---FOR EACH ROW
---BEGIN
---
---END;
---/
-
-
--- ******* Suppression => Vérification document rendu ******* --
---CREATE OR REPLACE TRIGGER tg_Borrow_VerifHasBeenReturned -- Doublon avec ci-dessus?
---BEFORE INSERT ON Borrow
---FOR EACH ROW
---BEGIN
---
---END;
---/
-
+CREATE OR REPLACE TRIGGER tg_Borrow_VerifIsBeingBorrowed
+BEFORE INSERT ON Borrow
+FOR EACH ROW
+Declare isBorrowed borrow.copy%type;
+BEGIN
+select copy
+into isBorrowed
+from borrow
+where :new.copy = borrow.copy;
+if isBorrowed is not null
+then raise_application_error('-200001','Already borrowed and not returned');
+end if;    
+END;
+/
 
 
 
@@ -120,7 +141,7 @@ END;
 --BEFORE INSERT ON Copy
 --FOR EACH ROW
 --BEGIN
---
+--    
 --END;
 --/
 
