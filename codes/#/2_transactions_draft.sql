fe60e05aae2c9e110db6e63f8c03095046e176b2
Florian <florian-1992@hotmail.fr>
2021-05-07 23:11:25 +0200

    J'ai commencé les transactions. Mais j'ai l'impression qu'il manque des choses... On pourrait peut-être faire ça en PL/SQL?

--- codes/#/2_transactions_draft.sql
+++ codes/#/2_transactions_draft.sql
@@ -1,4 +1,61 @@
--- ============================================================= --
--- ================ GESTION DES TRANSACTIONS =================== --
--- ============================================================= --
+-- ============================================================================== --
+-- ========================= GESTION DES TRANSACTIONS =========================== --
+-- ============================================================================== --
+-- Auteurs de ce script: Yann Berthelot, Amandine Fradet, Florian Legendre
+
+-- ******* Boîte à outils ******* --
+-- SET TRANSACTION READ WRITE; -- La transaction peut lire et écrire dans les tables
+-- SET TRANSACTION READ ONLY; -- On ne peut que lire
+
+-- Le "ISOLATION LEVEL" définit un comportement à la lecture mais n'empêche pas les insertions
+-- ou updates ou suppressions. Ici, empêche les lectures sales mais pas les lectures non répétables 
+-- ou les fantômes:
+-- SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+-- LOCK TABLE nom_table IN EXCLUSIVE MOVE --Verrou explicite en Ecriture
+-- LOCK TABLE nom_table IN SHARE MODE --Verrou explicite en Lecture
+
+-- SAVEPOINT toto; ROLLBACK TO toto;
+-- ROLLBACK; COMMIT;
+
+SHOW AUTOCOMMIT; --Affiche la valeur de la variable autocommit
+-- SET AUTOCOMMIT OFF; 
+
+---------------------------------------------------------------------------------
+--                                    Emprunt                                  --
+---------------------------------------------------------------------------------
+
+-- ******* Emprunt d'un même exemplaire par des membres de la librairie ******* --
+
+-- On suppose les membres de la librairie accèdent à la base de donnée par un formulaire ou
+-- une interface graphique. Le script ci-dessous correspond alors au script exécuté lors d'un
+-- emprunt quelconque. Il vise à empêcher des incohérences lors des emprunts simultanés. Pour
+-- illustrer son fonctionnement on peut ouvrir deux sqldeveloper avec deux utilisateurs différents 
+-- et exécuter le même script ci-dessous dans l'ordre.
+
+
+-- Pas nécessaire car Oracle l'ajoute implicitement à la première requête.
+-- COMMIT ferme alors le bloc tout aussi implicitement:
+SET TRANSACTION READ WRITE;
+-- On s'assure qu'il n'y a pas d'autocommit:
+SET AUTOCOMMIT OFF;
+
+-- On veut éviter la lecture non répétable. On pose donc un verrou dès le début 
+-- en lecture pour éviter de futures écritures dans la table Borrow:
+LOCK TABLE Chuxclub.Borrow IN SHARE MODE;
+
+-- L'utilisateur consulte les copies disponibles du Document qu'il souhaite emprunter:
+SELECT DISTINCT c.id 
+FROM Chuxclub.Copy c
+WHERE c.reference = 20 AND c.id NOT IN (SELECT copy FROM Chuxclub.Borrow WHERE return_date is null);
+
+-- On évite la lecture non répétable mais on risque maintenant des verrous mortels dans le cas où 
+-- on a deux transactions simultanées qui posent toutes les deux leurs verrous en lecture et qu'une d'entre elles 
+-- veut écrire:
+LOCK TABLE Chuxclub.Borrow IN EXCLUSIVE MODE;
+
+INSERT INTO Chuxclub.Borrow (borrower, copy, borrowing_date, return_date) VALUES (3, 20, sysdate, null);
+--ROLLBACK;
+        
+COMMIT;
 
--- codes/2_transactions.sql
+++ codes/2_transactions.sql
@@ -2,3 +2,57 @@
 -- ========================= GESTION DES TRANSACTIONS =========================== --
 -- ============================================================================== --
 -- Auteurs de ce script: Yann Berthelot, Amandine Fradet, Florian Legendre
+
+-- ******* Boîte à outils ******* --
+-- SET TRANSACTION READ WRITE; -- La transaction peut lire (avec quel niveau d'isolation???) et écrire dans les tables.
+-- SET TRANSACTION READ ONLY; -- On ne peut que lire.
+
+-- Le "ISOLATION LEVEL" définit un comportement à la lecture mais n'empêche pas les insertions
+-- ou updates ou suppressions. Ici, empêche les lectures sales mais pas les lectures non répétables 
+-- ou les fantômes:
+-- SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+-- LOCK TABLE nom_table IN EXCLUSIVE MOVE --Verrou explicite en Ecriture
+-- LOCK TABLE nom_table IN SHARE MODE --Verrou explicite en Lecture
+
+-- SAVEPOINT toto; ROLLBACK TO toto;
+-- ROLLBACK; COMMIT;
+
+-- SHOW AUTOCOMMIT; --Affiche la valeur de la variable autocommit
+-- SET AUTOCOMMIT OFF; 
+
+---------------------------------------------------------------------------------
+--                                    Emprunt                                  --
+---------------------------------------------------------------------------------
+
+-- ******* Emprunt d'un même exemplaire par des membres de la librairie ******* --
+
+-- On suppose les membres de la librairie accèdent à la base de donnée par un formulaire ou
+-- une interface graphique. Le script ci-dessous correspond alors au script exécuté lors d'un
+-- emprunt quelconque. Il vise à empêcher des incohérences lors des emprunts simultanés. Pour
+-- illustrer son fonctionnement on peut ouvrir deux sqldeveloper avec deux utilisateurs différents 
+-- et exécuter le même script ci-dessous dans l'ordre.
+
+
+-- Pas nécessaire car Oracle l'ajoute implicitement à la première requête.
+-- COMMIT ferme alors le bloc tout aussi implicitement:
+SET TRANSACTION READ WRITE NAME 'Emprunt';
+-- On s'assure qu'il n'y a pas d'autocommit:
+SET AUTOCOMMIT OFF;
+
+-- Le premier arrivé peut lancer la procédure d'emprunt, les autres attendent leur tour.
+-- On place le verrou en écriture avant la consultation des exemplaires disponibles afin
+-- d'éviter les lectures non répétables sur ces-derniers et les verrous mortels:
+LOCK TABLE Chuxclub.Borrow IN EXCLUSIVE MODE;
+
+-- L'utilisateur consulte les exemplaires disponibles du document qu'il souhaite emprunter:
+SELECT DISTINCT c.id 
+FROM Chuxclub.Copy c
+WHERE c.reference = 20 AND c.id NOT IN (SELECT copy FROM Chuxclub.Borrow WHERE return_date is null);
+
+-- L'utilisateur choisit un exemplaire et l'emprunte:
+INSERT INTO Chuxclub.Borrow (borrower, copy, borrowing_date, return_date) VALUES (3, 20, sysdate, null);
+
+-- L'utilisateur peut alors annuler son emprunt et/ou terminer sa transaction:
+--ROLLBACK;        
+COMMIT;
