da62700aad4b99e40020f442e91045a343aab2eb
Florian <florian-1992@hotmail.fr>
2021-05-08 15:11:59 +0200

    Template des réponses à la partie sur les optimisations ajouté

--- codes/6_optimizations.sql
+++ codes/6_optimizations.sql
@@ -3,3 +3,107 @@
 -- ============================================================================== --
 -- Auteurs de ce script: Yann Berthelot, Amandine Fradet, Florian Legendre
 
+
+---------------------------------------------------------------------------------
+--        Les index des requêtes correspondant au fichier 5_queries.sql        --
+---------------------------------------------------------------------------------
+
+-- ***** (1) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (2) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (3) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (4) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (5) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (6) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (7) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (8) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (9) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (10) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (11) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (12) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (13) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (14) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (15) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (16) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (17) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (18) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (19) ***** --
+-- Index choisi: 
+-- Motivations: 
+
+
+-- ***** (20) ***** --
+-- Index choisi: 
+-- Motivations: 
+
